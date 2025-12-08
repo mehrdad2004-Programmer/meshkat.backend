643a8428d00a923015c30c3b8a71b5b5ad254a2d
mehrdad kalateh arabi <mehrdad1234kalateh@gmail.com>
2025-12-08 02:39:18 -0800

    (feat) : basket migration was created

---
+++ database/migrations/2025_12_08_103648_basket.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(Schema::hasTable("basket")){
+            Schema::create("basket", function(Blueprint $table){
+                $table->id();
+                $table->string("tr_code");
+                $table->date("date");
+                $table->time("time");
+                $table->string("username");
+                $table->string("status");
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("basket");
+    }
+};
