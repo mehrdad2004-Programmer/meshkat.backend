32674fe29f0f5b7a691124a94f9e6df9e981ae73
mehrdad kalateh arabi <mehrdad1234kalateh@gmail.com>
2025-12-08 02:36:23 -0800

    (feat) : access_logs migration was added

---
+++ database/migrations/2025_12_08_103405_access_logs.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(Schema::hasTable("access_logs")){
+            Schema::create("access_logs", function(Blueprint $table){
+                $table->id();
+                $table->string("tr_code");
+                $table->string("username");
+                $table->date("date");
+                $table->time("time");
+                $table->string("ip_address");
+                $table->string("user_agent");
+                $table->string("op_type");
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("access_logs");
+    }
+};
