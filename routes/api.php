ece268de9965aaf9c27cfc68246abc1c4b2b8589
mehrdad kalateh arabi <mehrdad1234kalateh@gmail.com>
2025-12-09 09:26:51 -0800

    (feat) : routes were added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\v1\AuthController;
+use App\Http\Controllers\v1\BasketController;
+use App\Http\Controllers\v1\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +12,19 @@ Route::prefix("v1")->group(function(){
         Route::post("register", [AuthController::class, "register"]);
         Route::patch("changePassword", [AuthController::class, "changePassword"]);
         Route::patch("changeInfo", [AuthController::class, "changeInfo"]);
+    });
+
+    Route::prefix("products")->group(function(){
+        Route::get("get", [ProductsController::class, "readProducts"]);
+        Route::post("insert", [ProductsController::class, "insertProducts"]);
+        Route::patch("update", [ProductsController::class, "updateProducts"]);
+        Route::delete("delete", [ProductsController::class, "deleteProducts"]);
+    });
+
+    Route::prefix("basket")->group(function(){
+        Route::get("get", [BasketController::class, "readBasket"]);
+        Route::post("insert", [BasketController::class, "insertBasket"]);
+        Route::delete("delete", [BasketController::class, "deleteBasket"]);
+        Route::patch("update", [BasketController::class, "updateBasket"]);
     });
 });
